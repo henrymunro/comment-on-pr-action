99da44a1b9293f73741453b2c84e670f9252ff43
henrymunro <henrymunro73@gmail.com>
2019-09-19 15:53:09 +0100

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const prMessage: string = core.getInput("pr-message");
+
+    // Get client and context
+    const client: github.GitHub = new github.GitHub(
+      core.getInput("repo-token", { required: true })
+    );
+    const context = github.context;
+
+    if (!context.payload.pull_request) {
+      throw new Error("Action can only be used on PRs.");
+    }
+
+    const { number, owner, repo } = context.issue;
+
+    console.log(`Adding message: ${prMessage} to pull request ${number}`);
+
+    await client.pulls.createReview({
+      owner,
+      repo,
+      pull_number: number,
+      body: prMessage,
+      event: "COMMENT"
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
